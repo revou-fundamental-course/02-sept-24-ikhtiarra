24d9df41ae731c144d9869d000c499cdf03d3926
ikhtiarra <beghumsheza@gmail.com>
2024-09-12 21:00:53 +0700

    Hasilnya Amburadul

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-// Ini Javascript
+function luas(){
+    let panjangAlas = document.getElementById("panjang-alas").value;
+    let tinggi = document.getElementById("tinggi").value;
+    let hasilPerhitungan = parseInt(panjangAlas) + parseInt(tinggi);
+    document.write('Jawabannya adalah' + hasilPerhitungan)
+}
+
+function keliling(){
+    let sideA = document.getElementById("side-A").value;
+    let sideB = document.getElementById("side-B").value;
+    let sideC = document.getElementById("side-C").value;
+    let hasilPerhitungan = parseInt(sideA) + parseInt(sideB) + parseInt(SideC);
+    document.write('Jawabannya adalah' + hasilPerhitungan)
